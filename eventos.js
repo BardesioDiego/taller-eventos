0b060c286fdfb038af0fe7d8b5f7af15237b6424
BardesioDiego <diegobardesio@gmail.com>
2023-08-07 21:53:32 -0300

    Saludo, Div

--- eventos.js
+++ eventos.js
@@ -1,3 +1,11 @@
   function saludar(){
         alert("Hola!")
     }
+
+    addEventListener('DOMContentLoaded', inicio);
+function inicio() {
+    document.getElementById('saludo').addEventListener('click', presionoClick);}
+
+    function presionoClick() {
+        alert('Hola! Soy el div');
+    }
